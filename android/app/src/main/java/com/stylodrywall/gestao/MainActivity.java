12e8324541b0635e24318a21a5d74f0966e2ba74
mpereira356 <viny_gtb@hotmail.com>
2026-09-09 22:55:54 -0300

    att

---
+++ android/app/src/main/java/com/stylodrywall/gestao/MainActivity.java
@@ -0,0 +1,37 @@
+package com.stylodrywall.gestao;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private static final String PANEL_URL = "http://191.252.193.10:1515/admin";
+    private WebView webView;
+
+    @Override public void onCreate(Bundle state) {
+        super.onCreate(state);
+        webView = new WebView(this);
+        setContentView(webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        CookieManager.getInstance().setAcceptCookie(true);
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, false);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                String host = request.getUrl().getHost();
+                if ("191.252.193.10".equals(host)) return false;
+                return true;
+            }
+        });
+        if (state == null) webView.loadUrl(PANEL_URL); else webView.restoreState(state);
+    }
+    @Override protected void onSaveInstanceState(Bundle out) { webView.saveState(out); super.onSaveInstanceState(out); }
+    @Override public void onBackPressed() { if (webView.canGoBack()) webView.goBack(); else super.onBackPressed(); }
+}
